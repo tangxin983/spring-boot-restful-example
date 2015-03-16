870c3fb6d8689b7948d1a2cfff67440265afb51a
tangxin983 <tangxin983@163.com>
2015-03-16 15:57:39 +0800

    修改测试用例

--- src/test/java/com/github/tx/test/WebAppIntegrationTests.java
+++ src/test/java/com/github/tx/test/WebAppIntegrationTests.java
@@ -1,6 +1,5 @@
 package com.github.tx.test;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
@@ -27,76 +26,71 @@ import com.github.tx.entity.User;
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 @IntegrationTest
-// @ActiveProfiles("production")
+@ActiveProfiles("dev")
 public class WebAppIntegrationTests {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	RestTemplate template = new TestRestTemplate();
 
-	@Before
-	public void init() {
-		User user = new User("1@163.com", "用户1");
-		template.postForEntity("http://localhost:8080/user", user, String.class);
-		user = new User("2@163.com", "用户2");
-		template.postForEntity("http://localhost:8080/user", user, String.class);
-	}
-
-	@Test
-	public void testGet() {
-		ResponseEntity<String> entity = template.getForEntity(
-				"http://localhost:8080/user/1", String.class);
-		logger.info("======get======");
-		logger.info("status code is:{}", entity.getStatusCode());
-		logger.info("body is:{}", entity.getBody());
-		logger.info("======get======");
-	}
-
 	// @Test
 	public void testException() {
+		logger.info("======testException======");
 		logger.info("404 result is:"
 				+ template.getForObject("http://localhost:8080/404",
 						String.class));
 		logger.info("500 result is:"
 				+ template.getForObject("http://localhost:8080/err",
 						String.class));
+		logger.info("======testException======");
+	}
+
+	@Test
+	public void testGet() {
+		logger.info("======testGet======");
+		User user = template.getForObject("http://localhost:8080/user/1",
+				User.class);
+		logger.info("user1 is:{}", user.toString());
+		logger.info("======testGet======");
 	}
 
 	@Test
 	public void testPost() {
-		ResponseEntity<String> entity = template.postForEntity(
-				"http://localhost:8080/user", new User("post@163.com", "post"),
-				String.class);
-		logger.info("======post======");
-		logger.info("status code is:{}", entity.getStatusCode());
-		logger.info("body is:{}", entity.getBody());
-		logger.info("======post======");
-	}
-
-	@Test
-	public void testDelete() {
-		logger.info("======before del======");
-		logger.info("user2 is:"
-				+ template.getForEntity("http://localhost:8080/user/2",
-						String.class).getBody());
-		template.delete("http://localhost:8080/user/2");
-		logger.info("======after del======");
-		logger.info("user2 is:"
-				+ template.getForEntity("http://localhost:8080/user/2",
-						String.class).getBody());
-
+		User user = template.postForObject("http://localhost:8080/user",
+				new User("newuser@163.com", "newuser"), User.class);
+		logger.info("======testPost======");
+		logger.info("new user is:{}", user.toString());
+		logger.info("======testPost======");
 	}
 
 	@Test
 	public void testPut() {
-		logger.info("======before put======");
-		logger.info("user1 is:"
-				+ template.getForEntity("http://localhost:8080/user/1",
-						String.class).getBody());
-		template.put("http://localhost:8080/user/1", new User("put@163.com", "put"));
-		logger.info("======after put======");
-		logger.info("user1 is:"
-				+ template.getForEntity("http://localhost:8080/user/1",
-						String.class).getBody());
+		logger.info("======testPut======");
+		logger.info(
+				"before put,user1 is:{}",
+				template.getForObject("http://localhost:8080/user/1",
+						User.class).toString());
+		User updated = new User();
+		updated.setId(1l);
+		updated.setEmail("put@163.com");
+		updated.setUserName("putName");
+		template.put("http://localhost:8080/user", updated);
+		logger.info(
+				"after put,user1 is:{}",
+				template.getForObject("http://localhost:8080/user/1",
+						User.class).toString());
+		logger.info("======testPut======");
 	}
+
+	@Test
+	public void testDelete() {
+		logger.info("======testDelete======");
+		logger.info("before del,user2 is:"
+				+ template.getForObject("http://localhost:8080/user/2", User.class).toString());
+		template.delete("http://localhost:8080/user/2");
+		logger.info("after del,user2 is:" + template.getForObject("http://localhost:8080/user/2", User.class));
+		logger.info("======testDelete======");
+
+	}
+
 }
